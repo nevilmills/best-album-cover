2dd173fb9671eb19c0b1cac0911b4e26c696b291
Nevil Mills <nevilmills@gmail.com>
2025-07-12 09:20:35 -0700

    Add check to ensure two albums have been fetched

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,16 @@ export default async function Home() {
     return <div>loading...</div>;
   }
 
+  if (data.length < 2) {
+    return (
+      <div className="min-h-screen bg-background text-foreground p-8 flex flex-col items-center justify-center">
+        <h2 className="font-bold text-2xl self-center">
+          Not enough albums to compare
+        </h2>
+      </div>
+    );
+  }
+
   return (
     <div className="min-h-screen bg-background text-foreground p-8 flex flex-col items-center justify-center">
       <h2 className="font-bold text-2xl self-center">
