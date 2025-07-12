b41849aa3737770ac0ea4c0ea811e83e98908f7a
Nevil Mills <nevilmills@gmail.com>
2025-07-12 09:55:57 -0700

    Add github and results page links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,6 +27,22 @@ export default async function Home() {
       </h2>
       <div className="h-4" />
       <AlbumComparison initialAlbums={data} />
+      <div className="fixed bottom-0 left-0 w-full flex justify-center pb-4">
+        <div className="flex flex-row items-center space-x-2 ">
+          <a
+            href="https://github.com/nevilmills/best-album-cover"
+            className="text-muted-foreground hover:text-foreground"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+          <span className="text-muted-foreground">|</span>
+          <a href="" className="text-muted-foreground hover:text-foreground">
+            Results
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
