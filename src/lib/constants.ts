46011afbd8772c72978095a0aba2e4d5cde47303
Nevil Mills <nevilmills@gmail.com>
2025-07-11 18:38:49 -0700

    Add list of spotify ids for artists

---
+++ src/lib/constants.ts
@@ -0,0 +1,38 @@
+export const spotifyIds = [
+  "6mdiAmATAx73kdxrNrnlao", // Iron Maiden
+  "1Qp56T7n950O3EGMsSl81D", // Ghost
+  "5M52tdBnJaKSvOpJGz8mfZ", // Black Sabbath
+  "2tRsMl4eGxwoNabM08Dm4I", // Judas Priest
+  "2ye2Wgw4gimLv2eAKyk1NB", // Metallica :contentReference[oaicite:1]{index=1}
+  "1IQ2e1buppatiN1bxUVkrk", // Slayer :contentReference[oaicite:2]{index=2}
+  "0GDGKpJFhVpcjIGF8N6Ewt", // Gojira :contentReference[oaicite:3]{index=3}
+  "1DFr97A9HnbV3SKTJFu62M", // Motorhead
+  "6ZLTlhejhndI4Rh53vYhrY", // Ozzy Osbourne
+  "4CYeVo5iZbtYGBN4Isc3n6", // Dio
+  "0zfT626RwO6zN3RDYeRit5", // Black Label Society
+  "4CzUzn54Cp9TQr6a7JIlMZ", // Manowar
+  "34c4iQ5tkaZKu6Sv28BTde", // Danzig
+  "3JDIAtVrJdQ7GFOX26LYpv", // Accept
+  "3BVkDHWRvLJEyKdvhLbjsq", // Wasp
+  "2ye2Wgw4gimLv2eAKyk1NB", // Metallica
+  "1Yox196W7bzVNZI7RBaPnf", // Megadeth
+  "1IQ2e1buppatiN1bxUVkrk", // Slayer
+  "14pVkFUHDL207LzLHtSA18", // Pantera
+  "3JysSUOyfVs1UQ0UaESheP", // Anthrax
+  "6JW8wliOEwaDZ231ZY7cf4", // Sepultura
+  "3TOqt5oJwL9BE2NG9MEwDa", // Disturbed
+  "5eAWCfyUhZtHHtBdNk56l1", // System of a Down
+  "05fG473iIaoy82BF1aGhL8", // Slipknot
+  "6Ghvu1VvMGScGpOUJBAHNH", // Deftones
+  "3JFsVIxOn7STeilPICkkB2", // Lamb of God
+  "1Ffb6ejR6Fe5IamqA5oRUF", // Bring me the Horizon
+  "2NPduAUeLVsfIauhRwuft1", // Nightwish
+  "0ybFZ2Ab08V8hueghSXm6E", // Opeth
+  "1Dvfqq39HxvCJ3GvfeIFuT", // Mastodon
+  "2aaLAng2L2aWD2FClzwiep", // Dream Theater
+  "57ylwQTnFnIhJh4nu4rxCs", // In Flames
+  "4OAddazJM576euUnFSvXSL", // Lacuna Coil
+  "37394IP6uhnjIpsawpMu4l", // Killswitch Engage
+  "4f5V3PQ66nIrBCqugJtaGn", // Death
+  "5fuQrhMRYMtoO9uOlFad4P", // The Fall of Troy
+];
