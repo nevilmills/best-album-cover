f9390e070dff4e244cbf3bae31dae30fd8fa42ec
Nevil Mills <nevilmills@gmail.com>
2025-07-11 18:42:06 -0700

    Add image url domain to render images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
-export default function Home() {
+import { AlbumComparison } from "@/components/album-comparison";
+import { getRandomMetalAlbums } from "@/lib/spotify/getMetalAlbums";
+
+export default async function Home() {
+  const data = await getRandomMetalAlbums();
+
+  console.log("Fetched albums:", data);
+
+  if (!data || data.length === 0) {
+    return <div>loading...</div>;
+  }
+
   return (
     <div className="min-h-screen bg-background text-foreground p-8 flex flex-col items-center justify-center">
       <h2 className="font-bold text-2xl self-center">
         Which album cover is better?
       </h2>
       <div className="h-4" />
-      <div className="p-6 border rounded-md bg-card text-card-foreground h-96 w-[700px] flex flex-col items-center justify-center">
-        <div className="flex flex-row items-center justify-between w-full space-x-4">
-          <div className="w-1/2 h-[200px] bg-white">option 1</div>
-          <div>vs.</div>
-          <div className="w-1/2 h-[200px] bg-white">option 2</div>
-        </div>
-      </div>
+      <AlbumComparison initialAlbums={data} />
     </div>
   );
 }
